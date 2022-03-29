bc24d4fc2a37a9463032d76d871f3318fddb53bd
sowulff <sowu99@gmail.com>
2022-03-29 15:40:35 +0200

    messaged when submitted

--- pages/new/instructions.js
+++ pages/new/instructions.js
@@ -4,6 +4,38 @@ export default () => {
   return (
     <div>
       <h1>Så går det till!</h1>
+      <div>
+        <ol>
+          <li>
+            <h4>Skapa ett konto eller logga in</h4>
+            <p>
+              Skapa ett konto om dig själv för att få allt sparat på ett och
+              samma ställe. Självklart är det gratis.{" "}
+            </p>
+          </li>
+          <li>
+            <h4>Berätta om din bostad</h4>
+            <p>
+              Berätta om din bostad i vårat färdiga formulär för att du ska
+              kunna hitta rätt person som ska hyra. .{" "}
+            </p>
+          </li>
+          <li>
+            <h4>Lägg till dina bekvämligheter.</h4>
+            <p>
+              Beskriv vad som gör din bostad speciell - vare sig ifall det vore
+              en fin tomt, eller ett husdjur man skall ta hand om.
+            </p>
+          </li>
+          <li>
+            <h4>Dubbelkolla att allting stämmer.</h4>
+            <p>
+              Dubbelkolla att allting stämmer och puplicera din annons. Nu är
+              det bara att luta dig tillbaka och vänta på rätt hyresgäst.
+            </p>
+          </li>
+        </ol>
+      </div>
       <Link href="/new/register" replace>
         <a>nästa</a>
       </Link>
--- pages/new/register.js
+++ pages/new/register.js
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import { supabase } from "../../utils/supabaseClient";
-
+import Link from "next/link";
 function Register() {
+  const [submitted, setSubmitted] = useState(false);
   const handleSubmit = async (form) => {
     form.preventDefault();
     const { data, error } = await supabase.auth.signIn({
       email: document.getElementById("email").value,
+      redirectTo: "http://localhost:3000/new/options",
     });
-    if (error) console.log(error);
+    if (error) {
+      console.log(error);
+    } else {
+      setSubmitted(true);
+    }
     console.log(supabase.auth.user());
   };
+  if (submitted) {
+    return (
+      <div>
+        <h1>Check your email!</h1>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>
         Registrera dig för att spara dina uppgifter, eller fortsätt vidare för
         att testa.
       </h1>
-
+      <Link href="/new/options" replace>
+        <button>Fortsätt utan att registrera</button>
+      </Link>
       <form onSubmit={handleSubmit}>
         <label for="email">Email</label>
         <input
