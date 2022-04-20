cd5abbd8fa242efa4f9833d91d0d2fb2aff36552
sowulff <sowu99@gmail.com>
2022-04-20 17:14:05 +0200

    added logo mobile osv

--- components/FormWrapper.js
+++ components/FormWrapper.js
@@ -1,10 +1,11 @@
 import style from "../styles/form/components/form.module.scss";
 import LinksCompnent from "./LinksCompnent";
-
+import logoMobile from "../assets/logoMobile.svg";
 const FormWrapper = ({ title, description, children, next, prev }) => {
   return (
     <div className={style.container}>
       <div>
+        <img src={logoMobile.src} />
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
