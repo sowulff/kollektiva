8d18a3a39413cb8ac94e3f1886a65f5d06fd1859
sowulff <sowu99@gmail.com>
2022-04-21 22:53:48 +0200

    1

--- components/FormWrapper.js
+++ components/FormWrapper.js
@@ -2,18 +2,32 @@ import style from "../styles/form/components/form.module.scss";
 import LinksCompnent from "./LinksCompnent";
 import logoMobile from "../assets/logoMobile.svg";
 import logoFooterMobile from "../assets/logoFooterMobile.svg";
+import Image from "next/image";
 const FormWrapper = ({ title, description, children, next, prev }) => {
   return (
     <div className={style.container}>
       <div className={style.header}>
-        <img src={logoMobile.src} />
+        <div className={style.img}>
+          <Image
+            src={logoMobile.src}
+            alt="Kollektiva"
+            width="80"
+            height="100"
+          />
+        </div>
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
       <div>{children}</div>
       <LinksCompnent back={prev} next={next} />
       <div className={style.logoFooter}>
-        <img src={logoFooterMobile.src} />
+        <Image
+          // className={style.img}
+          src={logoFooterMobile.src}
+          alt="Kollektiva"
+          width="100"
+          height="100"
+        />
       </div>
     </div>
   );
